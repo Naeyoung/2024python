a11493bec4b44260f111238202641e7c30a83f5f
Naeyoung <33342518+Naeyoung@users.noreply.github.com>
2024-11-01 20:49:23 +0900

    Update 00_기온데이터분석.py

--- pages/00_기온데이터분석.py
+++ pages/00_기온데이터분석.py
@@ -1,12 +1,31 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-import plotly.express as px
 
 # CSV 파일에서 데이터를 불러오기
 data = pd.read_csv("daily_temp.csv")
 
-# 특정 지역의 일교차를 계산하는 함수
-def calculate_daily_temperature_range(data, location):
-    location_data = data[data['location'] == location]
-    location_data['temp_range'] = location_data['max_temp'] - location_data['min_temp']
-    return location_data[['date', 'location', 'max_temp', 'min_temp', 'temp_range']]
+
+# 일교차 열 생성하기
+df['일교차'] = df['최고기온'] - df['최저기온']
+pd.to_datetime(df['날짜'])
+df['날짜'] = pd.to_datetime(df['날짜'])
+
+df['년'] = df['날짜'].dt.year
+df['월'] = df['날짜'].dt.month
+df['일'] = df['날짜'].dt.day
+df['요일'] = df['날짜'].dt.weekday
+df
+
+
+def season (t) : 
+    if t>20 :
+        return '여름'
+    elif t<5 :
+        return '겨울'
+    else :
+        return '봄/가을'
+
+df['계절'] = df['평균기온'].apply(season)
+
+import matplotlib.pyplot as plt
+import koreanize_matplotlib
