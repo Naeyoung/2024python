41b3b882c25a9533e69244c390ee3cf0f62d9899
Naeyoung <33342518+Naeyoung@users.noreply.github.com>
2024-11-02 11:38:10 +0900

    Update 00_기온데이터분석.py

--- pages/00_기온데이터분석.py
+++ pages/00_기온데이터분석.py
@@ -1,1 +1,10 @@
+# pandas 라이브러리 불러오기
 import pandas as pd
+# 판다스에서 csv 데이터 읽어오기
+df = pd.read_csv('daily_temp.csv')
+
+# 타이틀과 설명 추가
+st.title('첫 프로젝트 일별 기온 분석')
+st.write("년도별 일별 기온 분석하기📊")
+
+df['일교차'] = df['최고기온']-df['최저기온']
